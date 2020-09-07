1e9ca60d9da18d89f09fb09dce3aee4f1729f081
sebagruia <sebastiangruia@gmail.com>
2020-09-07 10:04:15 +0200

    Added linkes for video from firebase storage

--- src/DATA.js
+++ src/DATA.js
@@ -1,45 +1,45 @@
-import Movie1 from "./assets/movies/PexelsVideos1572442.mp4";
-import Movie2 from "./assets/movies/PexelsVideos2325178.mp4";
-import Movie3 from "./assets/movies/PexelsVideos2675510.mp4";
-import Movie4 from "./assets/movies/PexelsVideos2818546.mp4";
-import Movie5 from "./assets/movies/Counting Money.mp4";
-import Movie6 from "./assets/movies/See.mp4";
-import Movie7 from "./assets/movies/flower.mp4";
+// import Movie1 from "./assets/movies/PexelsVideos1572442.mp4";
+// import Movie2 from "./assets/movies/PexelsVideos2325178.mp4";
+// import Movie3 from "./assets/movies/PexelsVideos2675510.mp4";
+// import Movie4 from "./assets/movies/PexelsVideos2818546.mp4";
+// import Movie5 from "./assets/movies/Counting Money.mp4";
+// import Movie6 from "./assets/movies/See.mp4";
+// import Movie7 from "./assets/movies/flower.mp4";
 
 export const movies = {
   Car_with_baloons: {
     id: `Car_with_baloons`,
-    src: Movie1,
+    src: "https://firebasestorage.googleapis.com/v0/b/video-media-player.appspot.com/o/movies%2FCar%20with%20Baloons.mp4?alt=media&token=1e6e04b5-2a44-41d0-91d1-fd8b5e42ff6d",
     active:"false",
   },
   Tea: {
     id: `Tea`,
-    src: Movie2,
+    src: "https://firebasestorage.googleapis.com/v0/b/video-media-player.appspot.com/o/movies%2FTea.mp4?alt=media&token=5f57c098-4384-4f8b-bb55-e5995da1a1eb",
     active:"false",
   },
   Surfing: {
     id: `Surfing`,
-    src: Movie3,
+    src: "https://firebasestorage.googleapis.com/v0/b/video-media-player.appspot.com/o/movies%2FSurfing.mp4?alt=media&token=0cdc67a8-73be-47a8-95c1-81dee05dbe32",
     active:"false",
   },
   City_view: {
     id: `City_view`,
-    src: Movie4,
+    src: "https://firebasestorage.googleapis.com/v0/b/video-media-player.appspot.com/o/movies%2FCity%20View.mp4?alt=media&token=646d803a-4b93-46fe-b7ad-1be65fea5c80",
     active:"false",
   },
   Counting_Money: {
     id: `Counting_Money`,
-    src: Movie5,
+    src: "https://firebasestorage.googleapis.com/v0/b/video-media-player.appspot.com/o/movies%2FCounting%20Money.mp4?alt=media&token=71a1e6de-fef6-4cee-9768-e2e5292ca9ea",
     active:"false",
   },
   See: {
     id: `See`,
-    src: Movie6,
+    src: "https://firebasestorage.googleapis.com/v0/b/video-media-player.appspot.com/o/movies%2FSee.mp4?alt=media&token=c1c473c6-a916-4b3f-8b61-a851216b9cf5",
     active:"false",
   },
   Flower: {
     id: `Flower`,
-    src: Movie7,
+    src: "https://firebasestorage.googleapis.com/v0/b/video-media-player.appspot.com/o/movies%2Fflower.mp4?alt=media&token=552e7649-b725-45ca-9054-29699eb88b88",
     active:"false",
   },
 };
