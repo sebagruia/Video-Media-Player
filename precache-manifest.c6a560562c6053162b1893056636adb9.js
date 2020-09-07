2a9ee79b374983994555543cd5bd5bf3f8828109
sebagruia <sebastiangruia@gmail.com>
2020-09-07 09:46:11 +0200

    Updates

---
+++ precache-manifest.c6a560562c6053162b1893056636adb9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f792f644bd2d20cb019c87c50131ca",
+    "url": "/Video-Media-Player/index.html"
+  },
+  {
+    "revision": "326feb5188ebc87e5cc3",
+    "url": "/Video-Media-Player/static/css/main.4ae0ece1.chunk.css"
+  },
+  {
+    "revision": "7d411cf9a2e96ef41b5d",
+    "url": "/Video-Media-Player/static/js/2.c59f4495.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Video-Media-Player/static/js/2.c59f4495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326feb5188ebc87e5cc3",
+    "url": "/Video-Media-Player/static/js/main.fcb430b4.chunk.js"
+  },
+  {
+    "revision": "83e17f3a8c3b538e7959",
+    "url": "/Video-Media-Player/static/js/runtime-main.a91d4da4.js"
+  },
+  {
+    "revision": "d220b8a85f74582c25ae2120f3d03398",
+    "url": "/Video-Media-Player/static/media/Counting Money.d220b8a8.mp4"
+  },
+  {
+    "revision": "04bf4c9a6f5d37a23b299e96bce384d9",
+    "url": "/Video-Media-Player/static/media/PexelsVideos1572442.04bf4c9a.mp4"
+  },
+  {
+    "revision": "5e4bc7d1134b3b9f6bf8a20e0292fe5f",
+    "url": "/Video-Media-Player/static/media/PexelsVideos2325178.5e4bc7d1.mp4"
+  },
+  {
+    "revision": "2176d4549e77566c310ea01318d72028",
+    "url": "/Video-Media-Player/static/media/PexelsVideos2675510.2176d454.mp4"
+  },
+  {
+    "revision": "d6fff52cbd80fe88441dd53970b0848c",
+    "url": "/Video-Media-Player/static/media/PexelsVideos2818546.d6fff52c.mp4"
+  },
+  {
+    "revision": "84add43ce105f3ccc0d341722b7a4ecb",
+    "url": "/Video-Media-Player/static/media/See.84add43c.mp4"
+  },
+  {
+    "revision": "25c37cf7ab5b95df3d40b4173227afb9",
+    "url": "/Video-Media-Player/static/media/flower.25c37cf7.mp4"
+  },
+  {
+    "revision": "ffbeba4b131dab41c865f446a43c06ef",
+    "url": "/Video-Media-Player/static/media/poster992.ffbeba4b.jpg"
+  }
+]);
