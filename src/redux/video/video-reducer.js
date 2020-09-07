826e72db77c621b6b36003be6d6f76eddd5e74c9
sebagruia <sebastiangruia@gmail.com>
2020-09-07 13:39:31 +0200

    Deleted unused functionality

--- src/redux/video/video-reducer.js
+++ src/redux/video/video-reducer.js
@@ -1,8 +1,7 @@
-import { ADD_REFERENCE_TO_VIDEO_IN_LIST, GET_ALL_VIDEOS_REFS } from "./video-action";
+import { ADD_REFERENCE_TO_VIDEO_IN_LIST} from "./video-action";
 
 const initialState = {
   refToVideoInList: "",
-  allVideosRefs:[]
 };
 
 export const videoReducer = (state = initialState, action) => {
@@ -11,11 +10,6 @@ export const videoReducer = (state = initialState, action) => {
       return {
         ...state,
         refToVideoInList: action.payload,
-      };
-    case GET_ALL_VIDEOS_REFS:
-      return {
-        ...state,
-        allVideosRefs: [...state.allVideosRefs, action.payload]
       };
     default:
       return state;
